9aa66f7f173fe1c95df37dca44915042e8890a50
mccoyLBI <kyle.b.scholl@gmail.com>
2015-10-09 09:16:36 -0400

    stuff

--- Scheduler 1.0/ViewController.swift
+++ Scheduler 1.0/ViewController.swift
@@ -9,17 +9,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	
+	
+	@IBAction func textEntered(sender: AnyObject) {
+		
+		//
+		
+		
+	}
+	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		print("Storyboard dimmension: 375 x 667");
+		updateSchedule()
+		
+/*
+		let defaults = NSUserDefaults.standardUserDefaults()
+		// Yup, problem is here. "...Property list invalid for format: 200 (property lists cannot contain objects of type 'CFType')..."
+		defaults.setObject(ABlock, forKey: "ABlock")
+		defaults.setObject(BBlock, forKey: "BBlock")
+		defaults.setObject(CBlock, forKey: "CBlock")
+		defaults.setObject(DBlock, forKey: "DBlock")
+		defaults.setObject(EBlock, forKey: "EBlock")
+		defaults.setObject(FBlock, forKey: "FBlock")
+		defaults.synchronize()
+*/
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	func updateSchedule(){
+		/*
+		let defaults = NSUserDefaults.standardUserDefaults()
+		ABlock.value = defaults.valueForKey("ABlock")
+		BBlock.value = defaults.valueForKey("BBlock")
+		CBlock.value = defaults.valueForKey("CBlock")
+		DBlock.value = defaults.valueForKey("DBlock")
+		EBlock.value = defaults.valueForKey("EBlock")
+		FBlock.value = defaults.valueForKey("FBlock") */
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	@IBAction func showAlertTapped(sender: AnyObject) {
+	//Create the AlertController
+	let actionSheetController: UIAlertController = UIAlertController(title: "Classes",
+	message: "A block: Robotics –––– ran into problems –> fix later",
+	preferredStyle: .Alert)
+	
+	//Create and add the Cancel action
+	let cancelAction: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
+	//Do some stuff
+	}
+	actionSheetController.addAction(cancelAction)
+	//Create and add an option action
+	//let nextAction: UIAlertAction = UIAlertAction(title: "Next", style: .Default) { action -> Void in
+	//Do some other stuff
+	}
+	
+	//Change the text color
+	//self.window?.tintColor = UIColor.orangeColor()
+	
+	//Present the AlertController
+	self.presentViewController(actionSheetController, animated: true, completion: nil)
+	}
+	*/
 }
+
+
 
